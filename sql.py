9282eee3c856c7324d456cfad4d87548062608b1
Chinmay Agrawal <chinmayagrawal775@gmail.com>
2024-01-26 17:42:28 +0530

    intial commit

---
+++ sql.py
@@ -0,0 +1,206 @@
+# 001, 15:41, Introduction to SQL
+# 002, 11:59, How to Download and Install MySQL in Windows 11
+# 003, 6:17, How to Download MYSQL
+# 004, 10:12, How to Install and Configure MYSQL
+# 005, 2:50, How to Start MYSQL
+# 006, 5:09, Create Database
+# 007, 1:36, Use Database
+# 008, 9:40, Create Table
+# 009, 4:22, Describe Table
+# 010, 4:17, Show Database and Table
+# 011, 6:37, INT and DEC Data Type
+# 012, 3:32, CHAR, VARCHAR and TEXT Data Type
+# 013, 2:24, DATE, DATATIME and TIMESTAMP Data Type
+# 014, 9:16, Example of All Data Type
+# 015, 3:53, Help and Support by MYSQL
+# 016, 9:57, INSERT INTO
+# 017, 4:45, INSERT INTO without specifying Column Name
+# 018, 3:22, INSERT INTO with Changing order of column
+# 019, 3:29, INSERT INTO Specified Column
+# 020, 3:39, INSERT INTO Multiple rows
+# 021, 4:37, SELECT All or Particular Column records
+# 022, 8:13, Single Quotes Problem
+# 023, 5:48, SELECT with LIMIT
+# 024, 6:15, WHERE Clause and equal operator
+# 025, 1:44, WHERE Clause and Not equal operator
+# 026, 5:44, WHERE Clause and Greater than less than
+# 027, 5:45, IS NULL and IS NOT NULL
+# 028, 2:56, AND operator
+# 029, 3:00, OR operator
+# 030, 9:18, Combination of AND & OR
+# 031, 7:17, IN Operator
+# 032, 1:49, NOT IN operator
+# 033, 3:53, BETWEEN Number
+# 034, 2:01, BETWEEN Text
+# 035, 3:51, BETWEEN Date
+# 036, 4:40, NOT BETWEEN Number
+# 037, 3:12, NOT BETWEEN Text
+# 038, 2:48, NOT BETWEEN Date
+039, 6:57, BETWEEN with IN
+040, 1:42, BETWEEN with NOT IN
+041, 13:20, LIKE with % WildCard
+042, 3:45, LIKE with _ Wildcard
+043, 1:09, LIKE with [charlist] Wildcard
+044, 2:08, NOT LIKE
+045, 4:02, ORDER BY decending order
+046, 1:56, ORDER BY Asecending order
+047, 8:55, NOT NULL
+048, 16:42, UNIQUE KEY
+049, 11:12, PRIMARY KEY
+050, 16:20, AUTO INCREMENT
+051, 4:05, Starting AUTO INCREMENT with a particular Number
+052, 3:52, AUTO INCREMENT without specifying column name
+053, 7:24, Alias
+054, 4:18, Arithmetic Operators
+055, 3:46, SELECT DISTINCT
+056, 6:48, ALTER TABLE for adding one column without constraints
+057, 3:20, ALTER TABLE for adding multiple columns without constraints
+058, 5:20, ALTER TABLE for Adding Column by Position without constraints
+059, 7:31, ALTER TABLE for Adding Column with constraints
+060, 4:31, ALTER TABLE for Adding Column by Position with constraints
+061, 3:40, ALTER TABLE for Adding Constraint in Column SQL
+062, 4:21, Change Column Name and Its Data Type WIthout Constraints
+063, 3:53, Change more than one column Name and its data type without constraints
+064, 6:53, CHANGE COLUMN name and its Data Type with constraint
+065, 3:49, MODIFY COLUMN Data Type and Its Size with or Without Constraint
+066, 5:49, DROP COLUMN with or without constraint
+067, 1:38, DROP TABLE or Delete Table from database
+068, 3:02, Delete Data of Table using TRUNCATE TABLE
+069, 1:41, Rename Table Name using RENAME TABLE
+070, 1:06, Alter Database using ALTER DATABASE
+071, 1:33, Delete Database using DROP DATABASE
+072, 2:20, SHOW COLUMNS
+073, 2:58, SHOW CREATE DATABASE
+074, 2:48, SHOW CREATE TABLE
+075, 11:12, Update Records in Table Part 1
+076, 3:23, Update Records in Table Part 2
+077, 9:02, UPDATE with CASE
+078, 4:58, DELETE recods in Table
+079, 5:48, Copy Old Table to New Table within same Database
+080, 6:31, Copy Old Table to New Table different Database
+081, 4:05, MIN and MAX Function
+082, 3:32, SUM and AVG Function
+083, 1:21, SQRT Function
+084, 19:18, DECIMAL Data Type
+085, 3:22, ROUND Function
+086, 6:17, COUNT Function
+087, 4:06, UPPER and LOWER Function
+088, 5:21, MID and LENGTH Function
+089, 5:18, CONCAT Function
+090, 0:55, REVERSE Function
+091, 1:27, NOW Function
+092, 8:18, GROUP BY
+093, 3:19, HAVING Clause
+094, 3:17, Why do we need Multi Table
+095, 9:08, Relationship between Tables
+096, 13:56, One to One Relationship
+097, 9:38, One to Many Relationship
+098, 10:39, Many to Many Relationship
+099, 4:49, What is Normalization
+100, 8:56, First Normal Form 1 NF
+101, 9:34, Second Normal Form 2 NF
+102, 8:15, Third Normal Form 3 NF
+103, 5:57, Short Note about Normalization
+104, 4:38, What is Foreign Key
+105, 8:58, Create Table with Foreign Key Theory
+106, 6:26, Create Table with Foreign Key Practical
+107, 2:41, How to find Constraint Name
+108, 2:52, Drop Foreign Key from Table
+109, 4:40, Add Foreign Key Constraint in a Table SQL
+110, 1:37, Unable to Delete Parent Table
+111, 4:10, Unable to Delete Parent Table records
+112, 1:02, ON DELETE Clause
+113, 6:29, ON DELETE CASCADE
+114, 5:15, ON DELETE SET NULL
+115, 3:52, ON DELETE NO ACTION or ON UPDATE RESTRICT
+116, 0:48, ON UPDATE Clause
+117, 3:31, ON UPDATE CASCADE
+118, 2:22, ON UPDATE SET NULL
+119, 3:44, ON UPDATE NO ACTION or ON UPDATE RESTRICT
+120, 9:11, Composite Key
+121, 5:51, Shorthand Notation
+122, 4:14, How to Join Tables
+123, 12:37, CROSS JOIN
+124, 16:06, INNER EQUIJOIN
+125, 5:25, INNER NON-EQUIJOIN
+126, 3:41, NATURAL JOIN
+127, 1:43, Outer Join
+128, 12:32, LEFT OUTER JOIN
+129, 3:35, RIGHT OUTER JOIN
+130, 1:24, FULL OUTER JOIN
+131, 0:56, What is Self Join
+132, 5:56, Why do we need Self Join
+133, 23:07, Self Join Example
+134, 8:26, Replacing NULL
+135, 12:11, UNION
+136, 2:17, UNION ALL
+137, 4:40, INTERSECT and EXCEPT
+138, 2:28, Solve error can't connect to mysql server on localhost
+
+
+
+# help <item>
+
+# help data manipulation
+#    CALL
+#    DELETE
+#    DO
+#    DUAL
+#    EXCEPT
+#    HANDLER
+#    IMPORT TABLE
+#    INSERT
+#    INSERT DELAYED
+#    INSERT SELECT
+#    INTERSECT
+#    JOIN
+#    LOAD DATA
+#    LOAD XML
+#    PARENTHESIZED QUERY EXPRESSIONS
+#    REPLACE
+#    SELECT
+#    TABLE
+#    UNION
+#    UPDATE
+#    VALUES STATEMENT
+
+# help data definition
+#    ALTER DATABASE
+#    ALTER EVENT
+#    ALTER FUNCTION
+#    ALTER INSTANCE
+#    ALTER LOGFILE GROUP
+#    ALTER PROCEDURE
+#    ALTER SCHEMA
+#    ALTER SERVER
+#    ALTER TABLE
+#    ALTER TABLESPACE
+#    ALTER VIEW
+#    CREATE DATABASE
+#    CREATE EVENT
+#    CREATE FUNCTION
+#    CREATE INDEX
+#    CREATE LOGFILE GROUP
+#    CREATE PROCEDURE
+#    CREATE SCHEMA
+#    CREATE SERVER
+#    CREATE SPATIAL REFERENCE SYSTEM
+#    CREATE TABLE
+#    CREATE TABLESPACE
+#    CREATE TRIGGER
+#    CREATE VIEW
+#    DROP DATABASE
+#    DROP EVENT
+#    DROP FUNCTION
+#    DROP INDEX
+#    DROP PROCEDURE
+#    DROP SCHEMA
+#    DROP SERVER
+#    DROP SPATIAL REFERENCE SYSTEM
+#    DROP TABLE
+#    DROP TABLESPACE
+#    DROP TRIGGER
+#    DROP VIEW
+#    FOREIGN KEY
+#    RENAME TABLE
+#    TRUNCATE TABLE
